f162bf4282bbc800f8c5465c1036a696cb11d354
FAKULTASTEKNIK1 <uninusfakultasteknik@gmail.com>
2024-08-15 11:28:02 +0700

    Update login.php

--- login.php
+++ login.php
@@ -1,20 +1,24 @@
-<!DOCTYPE HTML>
+<?php
+if (isset($_GET['pesan']))
+{
+    $pesan=$_GET['pesan'];
+}
+else{
+    $pesan="";
+}
+?>
 <html>
-    <head>
-        <title>Halaman Login</title>
-        <link rel="stylesheet" href="csslogin.css">
-    </head>
-   
     <body>
-        <div class="container">
-          <h1>Login</h1>
-            <form>
-                <label>Username</label><br>
-                <input type="text"><br>
-                <label>Password</label><br>
-                <input type="password"><br>
-                <button>Log in</button>
-            </form>
-        </div>     
+        <h1>LOG IN</h1>
+        <?php
+            if ($pesan=="kesalahan"){
+            echo "username atau password salah";
+        }
+        ?>
+        <form action="login_cek.php" method="post">
+            <input type="text" name="username" placeholder="username">
+            <input type="password" name="password" placeholder="password">
+            <button type="sumbit">Masuk</button>
+        </form>
     </body>
 </html>
